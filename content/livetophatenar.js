1fc6ff5e2de9f11d68ff9d7f4bc86e560f911135
taichino <taichino@gmail.com>
2009-12-25 18:00:43 +0900

    add option screen

--- content/livetophatenar.js
+++ content/livetophatenar.js
@@ -4,8 +4,37 @@ var TopHatenar = {
 	baseurl   : "http://tophatenar.com/view/",
 	imagepath : "chrome://livetophatenar/content/img/",
 	lasturl   : null,
+	activated : true,
 
+	init: function() {
+		prefs = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefBranch);
+		try {
+			if (prefs.getBoolPref("extentions.livetophatenar.enabled") == false) {
+				this.activated = false;
+				this.lasturl = null;
+				var base  = this.imagepath;
+				document.getElementById('subscribers-image').src = base + "disabled.png";
+				document.getElementById('bookmarks-image').src   = base + "na.png";
+				document.getElementById("livetophatenar-panel").setAttribute("tooltiptext", "機能は無効化されています");
+				document.getElementById('livetophatenar-context-toggle').label = "Enable LiveTophatenar";
+			}
+			else {
+				this.activated = true;
+				document.getElementById('livetophatenar-context-toggle').label = "Disable LiveTophatenar";
+			}
+		}
+		catch (e) {
+			// do nothing
+		}
+	},
+	
 	setScore: function(url) {
+		this.init();
+		
+		if (this.activated == false) {
+			return;
+		}
+		
 		var self = this;
 		var cur = window._content.document.location;
 		var url = url || (this.baseurl + cur);
@@ -22,8 +51,6 @@ var TopHatenar = {
 					self.setScore(self.host + data["candidate"]);
 				}
 				else {
-					Application.console.log(data["bookmarks"]);
-					Application.console.log(data["subscribers"]);
 					self._update(data);
 				}
 			}
@@ -32,20 +59,38 @@ var TopHatenar = {
 	},
 
 	goSrc: function() {
+		if (this.activated == false) {
+			return;
+		}
+		
         var windowManager = (Components.classes["@mozilla.org/appshell/window-mediator;1"]).getService();
 		var windowManagerInterface = windowManager.QueryInterface(Components.interfaces.nsIWindowMediator);
 		var browser = (windowManagerInterface.getMostRecentWindow("navigator:browser")).getBrowser();
-		var url = this.baseurl + window._content.document.location;
-		browser.addTab(url);
+		browser.addTab(this.lasturl);
 	},
-	
+
+	toggle: function() {
+		prefs = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefBranch);
+		try {
+			var cur = prefs.getBoolPref("extentions.livetophatenar.enabled");
+			if (cur) {
+				prefs.setBoolPref("extentions.livetophatenar.enabled", false);
+			}
+			else {
+				prefs.setBoolPref("extentions.livetophatenar.enabled", true);
+			}
+		}
+		catch (e) {
+			// do nothing
+		}
+		this.setScore();
+	},
+
 	_update: function(data) {
 		var base  = this.imagepath;
 		if (data.bookmarks) {
 			var rRank = this._getRank(data.subscribers);
 			var bRank = this._getRank(data.bookmarks);
-			Application.console.log(rRank);
-			Application.console.log(bRank);
 			document.getElementById('subscribers-image').src = base + rRank + ".png";
 			document.getElementById('bookmarks-image').src   = base + bRank + ".png";
 
@@ -105,3 +150,4 @@ var TopHatenar = {
 window.addEventListener("focus", function() {
 	TopHatenar.setScore();
 }, true);
+
